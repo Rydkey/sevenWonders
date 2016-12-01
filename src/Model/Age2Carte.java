a53bd910390d6301b78d364762bd7f32dc47210b
zliao <310449968@qq.com>
2016-12-01 13:10:48 +0100

    j'ai pas mis des donneés des cartes

---
+++ src/Model/Age2Carte.java
@@ -0,0 +1,31 @@
+package Model;
+
+/**
+ * Created by zliao on 01/12/16.
+ */
+public class Age2Carte {
+    CardGameModel haras = new CardGameModel(0,0,0,0,new int[]{0,0,0,0,0},0 ,"rouge");
+    CardGameModel baraquement = new CardGameModel(0,0,0,new int[]{0,0,0,0,0},0 ,"rouge");
+    CardGameModel champTir = new CardGameModel(0,0,0,0 ,new int[]{0,0,0,0,0},0 ,"rouge");
+    CardGameModel placeArmes = new CardGameModel(0,0,0 ,new int[]{0,0,0,0,0},0 ,"rouge");
+    CardGameModel muraille = new CardGameModel(0,0,0 ,new int[]{0,0,0,0,0},0 ,"rouge");
+    CardGameModel scierie = new CardGameModel(0,0,0,new int[]{0,0,0,0,0} ,new int[]{0,0,0,0,0},0 ,"marron");
+    CardGameModel briqueterie = new CardGameModel(0,0,0,new int[]{0,0,0,0,0},new int[]{0,0,0,0,0},0  ,"marron");
+    CardGameModel carriere = new CardGameModel(0,0,0 ,new int[]{0,0,0,0,0},new int[]{0,0,0,0,0},0 ,"marron");
+    CardScientifiqueModel bibliotheque = new CardScientifiqueModel(0,0,0 ,new int[]{0,0,0,0,0},0 ,0);
+    CardScientifiqueModel dispensaire = new CardScientifiqueModel(0,0,0 ,new int[]{0,0,0,0,0},0 ,0);
+    CardScientifiqueModel ecole = new CardScientifiqueModel(0,0,0,0,new int[]{0,0,0,0,0},0 ,0);
+    CardScientifiqueModel laboratoire = new CardScientifiqueModel(0,0,0 ,new int[]{0,0,0,0,0},0 ,0);
+    CardGameModel soufflerie = new CardGameModel(0,0,0 ,new int[]{0,0,0,0,0},new int[]{0,0,0,0,0},0,"grise");
+    CardGameModel sechoir = new CardGameModel(0,0,0 ,new int[]{0,0,0,0,0},new int[]{0,0,0,0,0},0 ,"grise");
+    CardGameModel foruum = new CardGameModel(0,0,0 ,new int[]{0,0,0,0,0},new int[]{0,0,0,0,0},0 ,"jaune");
+    CardGameModel caravanserail = new CardGameModel(0,0,0 ,new int[]{0,0,0,0,0},0 ,"jaune");
+    CardGameModel douanes = new CardGameModel(1,0,0 ,new int[]{0,0,0,0,0},0 ,"jaune");
+    CardGameModel brasserie = new CardGameModel(1,0,0 ,new int[]{0,0,0,0,0},0 ,"jaune");
+    CardGameModel tribunal = new CardGameModel(0,0,0,0 ,new int[]{0,0,0,0,0},0 ,"bleu");
+    CardGameModel statue = new CardGameModel(0,0,0,0 ,new int[]{0,0,0,0,0},0 ,"bleu");
+    CardGameModel temple = new CardGameModel(0,0,0,0 ,new int[]{0,3,0,0,0},0 ,"bleu");
+    CardGameModel aqueduc = new CardGameModel(0,0,0,0 ,new int[]{0,0,0,0,0},0 ,"bleu");
+    CardGameModel rostres = new CardGameModel(0,0,0,0 ,new int[]{0,0,0,0,0},0 ,"bleu");
+
+}

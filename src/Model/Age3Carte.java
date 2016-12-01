ed79aa1d1e265109204fc0126e681446dc6a3646
zliao <310449968@qq.com>
2016-12-01 13:20:13 +0100

    j'ai pas mis des donneés des cartes

---
+++ src/Model/Age3Carte.java
@@ -0,0 +1,27 @@
+package Model;
+
+/**
+ * Created by zliao on 01/12/16.
+ */
+public class Age3Carte {
+    CardGameModel aresenal = new CardGameModel(0,0,0,0,new int[]{0,0,0,0,0},0 ,"rouge");
+    CardGameModel pretoire = new CardGameModel(0,0,0,new int[]{0,0,0,0,0},0 ,"rouge");
+    CardGameModel fortifications = new CardGameModel(0,0,0,0 ,new int[]{0,0,0,0,0},0 ,"rouge");
+    CardGameModel atelierDesiege = new CardGameModel(0,0,0 ,new int[]{0,0,0,0,0},0 ,"rouge");
+    CardGameModel cirque = new CardGameModel(0,0,0 ,new int[]{0,0,0,0,0},0 ,"rouge");
+    CardScientifiqueModel academie = new CardScientifiqueModel(0,0,0 ,new int[]{0,0,0,0,0},0 ,0);
+    CardScientifiqueModel etude = new CardScientifiqueModel(0,0,0 ,new int[]{0,0,0,0,0},0 ,0);
+    CardScientifiqueModel universite = new CardScientifiqueModel(0,0,0,0,new int[]{0,0,0,0,0},0 ,0);
+    CardScientifiqueModel observatoire = new CardScientifiqueModel(0,0,0 ,new int[]{0,0,0,0,0},0 ,0);
+    CardGameModel chambreDecommerce = new CardGameModel(0,0,0 ,new int[]{0,0,0,0,0},new int[]{0,0,0,0,0},0 ,"jaune");
+    CardGameModel port = new CardGameModel(0,0,0 ,new int[]{0,0,0,0,0},0 ,"jaune");
+    CardGameModel armurerie = new CardGameModel(1,0,0 ,new int[]{0,0,0,0,0},0 ,"jaune");
+    CardGameModel phare = new CardGameModel(1,0,0 ,new int[]{0,0,0,0,0},0 ,"jaune");
+    CardGameModel arene = new CardGameModel(1,0,0 ,new int[]{0,0,0,0,0},0 ,"jaune");
+    CardGameModel palace = new CardGameModel(0,0,0,0 ,new int[]{0,0,0,0,0},0 ,"bleu");
+    CardGameModel hotelDeville = new CardGameModel(0,0,0,0 ,new int[]{0,0,0,0,0},0 ,"bleu");
+    CardGameModel obelisque = new CardGameModel(0,0,0,0 ,new int[]{0,3,0,0,0},0 ,"bleu");
+    CardGameModel jardins = new CardGameModel(0,0,0,0 ,new int[]{0,0,0,0,0},0 ,"bleu");
+    CardGameModel pantheon = new CardGameModel(0,0,0,0 ,new int[]{0,0,0,0,0},0 ,"bleu");
+    CardGameModel senat = new CardGameModel(0,0,0,0 ,new int[]{0,0,0,0,0},0 ,"bleu");
+}

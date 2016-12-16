5571dc92f4067f7f0fb3c54350846738f52ad24e
antoine.py@univ-fcomte.fr <guildantoine@hotmail.fr>
2016-12-16 11:03:48 +0100

    vue merveille image, essai 1

---
+++ src/Controleur/ListenerPlateauCarte.java
@@ -0,0 +1,7 @@
+package Controleur;
+
+/**
+ * Created by ANTOINE on 16/12/2016.
+ */
+public class ListenerPlateauCarte {
+}
---
+++ src/Vue/ConteneurPlateauCarte.java
@@ -0,0 +1,27 @@
+package Vue;
+
+import javax.swing.*;
+
+/**
+ * Created by ANTOINE on 16/12/2016.
+ */
+public class ConteneurPlateauCarte extends JPanel {
+    private Fenetre fen;
+    JPanel conteneurJLabelCarte;
+    JPanel global;
+    JLabel tabCarte[] = new JLabel[20];
+
+    public ConteneurPlateauCarte(Fenetre fenetre){
+        this.fen=fenetre;
+        initAttribut();
+        addWidgets();
+
+    }
+    private void initAttribut(){
+        global = new JPanel();
+        global.setLayout(new BoxLayout(global,B));
+    }
+    private void addWidgets(){
+
+    }
+}
